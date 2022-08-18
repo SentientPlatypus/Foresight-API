d059a0ba81a61221548bcd9ee22aef2a51a4c0c0
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2022-08-18 13:02:04 -0400

    Replit sucks

--- stockAPIWhenTensorFlowWontInstall.py
+++ stockAPIWhenTensorFlowWontInstall.py
@@ -78,64 +78,65 @@ def getFinancials(ticker:str) -> dict:
 def getNumbers(ticker:str):
     toDisplay:pd.DataFrame = yf.download(ticker, period="max", progress=False)
 
-    df:pd.DataFrame = yf.download(ticker, period="1y", progress=False)
-    y = df['Close'].fillna(method='ffill')
-    y = y.values.reshape(-1, 1)
+    # df:pd.DataFrame = yf.download(ticker, period="1y", progress=False)
+    # y = df['Close'].fillna(method='ffill')
+    # y = y.values.reshape(-1, 1)
 
-    # scale the data
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    scaler = scaler.fit(y)
-    y = scaler.transform(y)
+    # # scale the data
+    # scaler = MinMaxScaler(feature_range=(0, 1))
+    # scaler = scaler.fit(y)
+    # y = scaler.transform(y)
 
-    # generate the input and output sequences
-    n_lookback = 60  # length of input sequences (lookback period)
-    n_forecast = 90  # length of output sequences (forecast period)
+    # # generate the input and output sequences
+    # n_lookback = 60  # length of input sequences (lookback period)
+    # n_forecast = 90  # length of output sequences (forecast period)
 
-    X = []
-    Y = []
+    # X = []
+    # Y = []
 
-    for i in range(n_lookback, len(y) - n_forecast + 1):
-        X.append(y[i - n_lookback: i])
-        Y.append(y[i: i + n_forecast])
+    # for i in range(n_lookback, len(y) - n_forecast + 1):
+    #     X.append(y[i - n_lookback: i])
+    #     Y.append(y[i: i + n_forecast])
 
-    X = np.array(X)
-    Y = np.array(Y)
+    # X = np.array(X)
+    # Y = np.array(Y)
 
-    # fit the model
-    model = Sequential()
-    model.add(LSTM(units=50, return_sequences=True, input_shape=(n_lookback, 1)))
-    model.add(LSTM(units=50))
-    model.add(Dense(n_forecast))
+    # # fit the model
+    # model = Sequential()
+    # model.add(LSTM(units=50, return_sequences=True, input_shape=(n_lookback, 1)))
+    # model.add(LSTM(units=50))
+    # model.add(Dense(n_forecast))
 
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.fit(X, Y, epochs=100, batch_size=32, verbose=0)
+    # model.compile(loss='mean_squared_error', optimizer='adam')
+    # model.fit(X, Y, epochs=100, batch_size=32, verbose=0)
 
-    # generate the forecasts
-    X_ = y[- n_lookback:]  # last available input sequence
-    X_ = X_.reshape(1, n_lookback, 1)
+    # # generate the forecasts
+    # X_ = y[- n_lookback:]  # last available input sequence
+    # X_ = X_.reshape(1, n_lookback, 1)
 
-    Y_ = model.predict(X_).reshape(-1, 1)
-    Y_ = scaler.inverse_transform(Y_)
+    # Y_ = model.predict(X_).reshape(-1, 1)
+    # Y_ = scaler.inverse_transform(Y_)
 
-    # organize the results in a data frame
-    df_past = df[['Close']].reset_index()
-    df_past.rename(columns={'index': 'Date', 'Close': 'Actual'}, inplace=True)
-    df_past['Date'] = pd.to_datetime(df_past['Date'])
-    df_past['Forecast'] = np.nan
-    df_past['Forecast'].iloc[-1] = df_past['Actual'].iloc[-1]
+    # # organize the results in a data frame
+    # df_past = df[['Close']].reset_index()
+    # df_past.rename(columns={'index': 'Date', 'Close': 'Actual'}, inplace=True)
+    # df_past['Date'] = pd.to_datetime(df_past['Date'])
+    # df_past['Forecast'] = np.nan
+    # df_past['Forecast'].iloc[-1] = df_past['Actual'].iloc[-1]
 
-    df_future = pd.DataFrame(columns=['Date', 'Actual', 'Forecast'])
-    df_future['Date'] = pd.date_range(start=df_past['Date'].iloc[-1] + pd.Timedelta(days=1), periods=n_forecast)
-    df_future['Forecast'] = Y_.flatten()
-    df_future['Actual'] = np.nan
+    # df_future = pd.DataFrame(columns=['Date', 'Actual', 'Forecast'])
+    # df_future['Date'] = pd.date_range(start=df_past['Date'].iloc[-1] + pd.Timedelta(days=1), periods=n_forecast)
+    # df_future['Forecast'] = Y_.flatten()
+    # df_future['Actual'] = np.nan
 
-    results:pd.DataFrame = df_past.append(df_future).set_index('Date')
-    results = results[results["Forecast"].notna()]
-    results["Open"] = results["Forecast"]
-    results["High"] = results["Forecast"]
-    results["Low"] = results["Forecast"]
-    results["Close"] = results["Forecast"]
-    final = pd.concat([toDisplay, results], sort=False, join="inner")
+    # results:pd.DataFrame = df_past.append(df_future).set_index('Date')
+    # results = results[results["Forecast"].notna()]
+    # results["Open"] = results["Forecast"]
+    # results["High"] = results["Forecast"]
+    # results["Low"] = results["Forecast"]
+    # results["Close"] = results["Forecast"]
+    # final = pd.concat([toDisplay, results], sort=False, join="inner")
+    final = toDisplay
     csv_df = final.to_csv()
     newStr = str(csv_df)
     return newStr[24:]
